841a2ef70f523b05b95ec08359c4353f5d04a740
Luis Felipe <luisgameweb@gmail.com>
2025-04-15 17:01:31 -0300

    Primeiro Commit

---
+++ Assets/Script/EscreveTexto.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class EscreveTexto : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI texto;
+    [TextArea]
+    [SerializeField] private string mensagemCompleta;
+    [SerializeField] private float velocidadeDigitacao = 0.05f;
+
+    private bool escrevendo = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        texto.text = "";
+        StartCoroutine(DigitarTexto());
+    }
+
+    private IEnumerator DigitarTexto()
+    {
+        escrevendo = true;
+
+        foreach (char letra in mensagemCompleta)
+        {
+            texto.text += letra;
+            yield return new WaitForSeconds(velocidadeDigitacao);
+        }
+        escrevendo = false;
+    }
+    
+    
+}
---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private AudioSource audio;
+    [SerializeField] private AudioClip som;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       audio = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+public void Jogar()
+    {
+        TocarSom();
+        Invoke("SelecionaPersonagens", 1f);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+    public void Druida()
+    {
+        SceneManager.LoadScene("Druida");
+    }
+    public void Guerreiro()
+    {
+        SceneManager.LoadScene("Samurai");
+    }
+    public void Bruxo()
+    {
+        SceneManager.LoadScene("Bruxo");
+    }
+    public void TocarSom()
+    {
+        audio.PlayOneShot(som);
+    }
+    public void SelecionaPersonagens()
+    {
+        SceneManager.LoadScene("SelecionaPersonagens");
+    }
+    public void ProximaFase()
+    {
+        SceneManager.LoadScene("ProximaFase");
+    }
+    public void BatalhaSamuraiOrc()
+    {
+        SceneManager.LoadScene("BatalhaSamuraiOrc");
+    }
+    
+    
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
